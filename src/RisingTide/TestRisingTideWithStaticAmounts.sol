9560a55a1dbcbe6445a08582fca8db2092924aa7
Miguel Palhas <mpalhas@gmail.com>
2025-11-13 17:50:30 +0000

    forge fmt

--- src/RisingTide/TestRisingTideWithStaticAmounts.sol
+++ src/RisingTide/TestRisingTideWithStaticAmounts.sol
@@ -9,46 +9,25 @@ contract TestRisingTideWithStaticAmounts is RisingTide {
     uint256 private immutable _totalAvailable;
     mapping(uint256 => uint256) allZeros;
 
-    constructor(
-        uint256 _totalInvestors,
-        uint256 _amountPerInvestor,
-        uint256 __totalAvailable
-    ) {
+    constructor(uint256 _totalInvestors, uint256 _amountPerInvestor, uint256 __totalAvailable) {
         totalInvestors = _totalInvestors;
         amountPerInvestor = _amountPerInvestor;
         _totalAvailable = __totalAvailable;
     }
 
-    function investorCount()
-        public
-        view
-        override(RisingTide)
-        returns (uint256)
-    {
+    function investorCount() public view override(RisingTide) returns (uint256) {
         return totalInvestors;
     }
 
-    function investorAmountAt(
-        uint256 i
-    ) public view override(RisingTide) returns (uint256) {
+    function investorAmountAt(uint256 i) public view override(RisingTide) returns (uint256) {
         return allZeros[i] + amountPerInvestor;
     }
 
-    function risingTide_totalAllocatedUncapped()
-        public
-        view
-        override(RisingTide)
-        returns (uint256)
-    {
+    function risingTide_totalAllocatedUncapped() public view override(RisingTide) returns (uint256) {
         return amountPerInvestor * totalInvestors;
     }
 
-    function risingTide_totalCap()
-        public
-        view
-        override(RisingTide)
-        returns (uint256)
-    {
+    function risingTide_totalCap() public view override(RisingTide) returns (uint256) {
         return _totalAvailable;
     }
 
