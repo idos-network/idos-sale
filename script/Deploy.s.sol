80cc9eb892be361af2d30ac116c915a54f3905c3
Miguel Palhas <mpalhas@gmail.com>
2025-11-17 16:32:50 +0000

    wip

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,11 +5,11 @@ import {Script} from "forge-std/Script.sol";
 import {Sale} from "src/Sale.sol";
 
 contract Deploy is Script {
-    address usdc = address(0x1); // TODO
+    address usdc = address(0xaf88d065e77c8cC2239327C5EDb3A432268e5831); // TODO this is USDC. not sure if we want USDC.e instead?
     uint256 rate = 0; // TODO
     uint256 start = 0; // TODO
     uint256 end = 0; // TODO
-    uint256 totalTokensForSale = 0; // TODO
+    uint256 totalTokensForSale = 25_000_000 ether; // TODO
     uint256 minTarget = 0; // TODO
     uint256 maxTarget = 0; // TODO
     uint256 startRegistration = 0; // TODO
