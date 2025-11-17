6f553624dbc6662f1cb38a12b0c5df202611d8b4
Miguel Palhas <mpalhas@gmail.com>
2025-11-17 17:17:11 +0000

    wip

--- src/Sale.sol
+++ src/Sale.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 import {IERC20} from "@openzeppelin/token/ERC20/IERC20.sol";
-import "forge-std/console.sol";
 import {IERC20Metadata} from "@openzeppelin/token/ERC20/extensions/IERC20Metadata.sol";
 import {SafeERC20} from "@openzeppelin/token/ERC20/utils/SafeERC20.sol";
 import {AccessControl} from "@openzeppelin/access/AccessControl.sol";
