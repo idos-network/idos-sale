31b3e5db3b843ea5a171567abc4aab187e14b770
Miguel Palhas <mpalhas@gmail.com>
2025-11-19 15:32:11 +0000

    yarn wagmi:generate (#23)

---
+++ wagmi.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "@wagmi/cli";
+import { foundry, react } from "@wagmi/cli/plugins";
+
+export default defineConfig({
+	out: "wagmi.generated.ts",
+	plugins: [
+		foundry({
+			project: "./",
+			exclude: ["MockERC20.sol", "Sale.d.sol", "IERC20.sol"],
+			// legacy name, for compatibility reasons
+			namePrefix: "Ctznd",
+			deployments: {
+				Sale: {
+					31337: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+					421614: "0x5e4bf20bbb995bd6a30d1e384c85a56b5ad703e4",
+				},
+			},
+		}),
+		react(),
+	],
+});
